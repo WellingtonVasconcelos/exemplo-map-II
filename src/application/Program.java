625dc74050dc2355f90495c7d3a72c4eda0b5bab
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-03-09 18:37:40 -0300

    Exemplo MAP com HashMap + Equals + hashCode

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import entities.Product;
+
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Map<Product, Double> stock = new HashMap<>();
+
+		Product prod1 = new Product("Tv", 900.0);
+		Product prod2 = new Product("Notebook", 1200.0);
+		Product prod3 = new Product("Tablet", 400.0);
+
+		stock.put(prod1, 10000.0);
+		stock.put(prod2, 20000.0);
+		stock.put(prod3, 15000.0);
+
+		Product ps = new Product("Tv", 900.0);
+
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
 
 	}
-
 }
